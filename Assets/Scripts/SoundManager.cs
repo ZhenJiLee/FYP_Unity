941b18fe27a8b72ba93c554d211922f4d66141b0
ZhenJiLee <zhenji2000.zl@gmail.com>
2024-11-13 01:54:36 +0800

    Options setting-sound control
    next- graphic setting

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+
+    void Start()
+    {
+        // 检查是否有保存的音量值，如果没有则设置默认值为50
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 50); // 默认值设为50（即0.5）
+            volumeSlider.value = 50; // 设置滑块初始值为50
+        }
+        else
+        {
+            Load(); // 如果有保存的音量值，加载并应用
+        }
+
+        ApplyVolume(); // 初始应用音量
+    }
+
+    public void ChangeVolume()
+    {
+        ApplyVolume(); // 更新全局音量
+        Save(); // 保存当前音量值
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume"); // 读取0-100的值
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value); // 保存0-100的值
+    }
+
+    private void ApplyVolume()
+    {
+        AudioListener.volume = volumeSlider.value / 100f; // 将滑块值缩放到0-1范围并应用
+    }
+}
