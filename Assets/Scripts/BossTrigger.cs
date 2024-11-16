088d7855f030e9d8395b25d26c9c65fe9b32cf5f
ZhenJiLee <zhenji2000.zl@gmail.com>
2024-11-17 00:17:35 +0800

    level1 health bug

---
+++ Assets/Scripts/BossTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTrigger : MonoBehaviour
+{
+    public GameObject bossCutsceneCanvas; // 用于显示过场动画的 Canvas
+    private bool triggered = false; // 防止重复触发
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+            bossCutsceneCanvas.SetActive(true); // 显示过场动画
+            Invoke("StartBossBattle", 3f); // 3秒后开始BOSS战
+        }
+    }
+
+    private void StartBossBattle()
+    {
+        bossCutsceneCanvas.SetActive(false); // 隐藏过场动画
+        // 在这里可以调用 BOSS 战的开始逻辑
+        Debug.Log("Boss Battle Started!");
+    }
+}
